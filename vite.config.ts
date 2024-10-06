b8c083492f27b7a9bac7a563b193245a83ce5176
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-10-06 08:42:41 -0300

    style: :lipstick: Add Stylelint and plugin PurgueCSS

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import react from '@vitejs/plugin-react-swc';
 import Oxlint from 'unplugin-oxlint/vite';
 import { defineConfig } from 'vite';
+import pluginPurgeCss from 'vite-plugin-purgecss-updated-v5';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// eslint-disable-next-line new-cap
-	plugins: [react(), Oxlint()],
+	plugins: [
+		react(),
+		// eslint-disable-next-line new-cap
+		Oxlint(),
+		pluginPurgeCss({
+			variables: true,
+		}),
+	],
 });

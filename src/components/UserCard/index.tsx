cf0ed21263964043b2f838a68b53dee596cc78e0
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-10-08 04:31:46 -0300

    fix: :bug: Fix sonar cloud 'Maintainability'

--- src/components/UserCard/index.tsx
+++ src/components/UserCard/index.tsx
@@ -1,7 +1,7 @@
 import styles from './styles.module.css';
 import { User } from './useUsers';
 
-function UserCard({ user }: { user: Readonly<User> }): JSX.Element {
+function UserCard({ user }: Readonly<{ user: User }>): JSX.Element {
 	return (
 		<div className={styles['m-user-card']}>
 			<p>{user.name}</p>

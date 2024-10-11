c329749e6d5e3484a488907a7b99df3110f59c2e
Galeed Gutierrez <galeedeveloper@gmail.com>
2024-10-10 22:18:34 -0300

    chore: :wrench: Add alias in Vite and reorder alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath } from 'node:url';
+
 import react from '@vitejs/plugin-react-swc';
 import Oxlint from 'unplugin-oxlint/vite';
 import { defineConfig } from 'vite';
@@ -19,6 +21,34 @@ export default defineConfig(({ mode }) => {
 		server: {
 			open: true,
 			host: true,
+		},
+		resolve: {
+			alias: {
+				'@src': fileURLToPath(new URL('src/', import.meta.url)),
+				'@tests': fileURLToPath(new URL('tests/', import.meta.url)),
+				'@e2e': fileURLToPath(new URL('e2e/', import.meta.url)),
+				'@components': fileURLToPath(
+					new URL('src/components/', import.meta.url),
+				),
+				'@context': fileURLToPath(
+					new URL('src/context/', import.meta.url),
+				),
+				'@data': fileURLToPath(new URL('src/data/', import.meta.url)),
+				'@hooks': fileURLToPath(new URL('src/hooks/', import.meta.url)),
+				'@templates': fileURLToPath(
+					new URL('src/templates/', import.meta.url),
+				),
+				'@styles': fileURLToPath(
+					new URL('src/styles/', import.meta.url),
+				),
+				'@utils': fileURLToPath(new URL('src/utils/', import.meta.url)),
+				'@images': fileURLToPath(
+					new URL('src/assets/images/', import.meta.url),
+				),
+				'@fonts': fileURLToPath(
+					new URL('src/assets/fonts/', import.meta.url),
+				),
+			},
 		},
 		test: {
 			clearMocks: true, // Automatically clear mock calls, instances, contexts and results before every test
